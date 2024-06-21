45b8af36845acadff1c4f1ee6c9d2d63093a7715
Manuel <m.p.porroa@maxedv-beratung.de>
2024-06-21 16:12:19 +0200

    defining routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+//Import Routes
+import taskRoutes from './routes/tasks.routes';
 
 const app = express();
 
@@ -8,5 +10,8 @@ app.use(cors()); // to connect with other servers
 app.use(morgan('dev')); // to see the request
 app.use(express.json());
 app.set('port', process.env.PORT || 3000);
+
+//Use imported route
+app.use(taskRoutes);
 
 export default app;
---
+++ src/routes/tasks.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/tasks', (req, res) => res.send('Hello world!'));
+
+router.get('/tasks/count', (req, res) => res.send('Hello world!'));
+
+router.post('/tasks', (req, res) => res.send('Hello world!'));
+
+router.get('/tasks/:id', (req, res) => res.send('Hello world!'));
+
+router.delete('/tasks/:id', (req, res) => res.send('Hello world!'));
+
+router.put('/tasks/:id', (req, res) => res.send('Hello world!'));
+
+export default router;
